0ad43d33d61a5ad1951613e8126bbfc0095e5021
btvh1994 <ymasterb@yahoo.com>
2017-09-19 18:26:16 +0000

    Initial commit

--- functions/connection.php
+++ functions/connection.php
@@ -1,5 +1,16 @@
 <?php
 
-$link= mysqli_connect("localhost", "adminuser", "password", "sqlinjectionsite");
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    
+    if (empty($url["user"]))
+        $link= mysqli_connect("localhost", "adminuser", "password", "sqlinjectionsite");
+    else {
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+        
+        $link = mysqli_connect($server, $username, $password, $db);
+    }
 
 ?>
